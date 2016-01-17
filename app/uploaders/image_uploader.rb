ec065db69369aca8a607443728645c8f1a18a220
chingyao cheng <cchegn0215@gmail.com>
2016-01-17 23:52:45 +0800

    add default image to carrierwave

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -17,15 +17,16 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
+  def default_url
+    'food.png'
   #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  # ActionController::Base.helpers.asset_path("food.jpg")
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
+  # process :scale => [50, 50]
   #
   # def scale(width, height)
   #   # do something
@@ -33,7 +34,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [50, 50]
+    process :resize_to_fit => [100, 100]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
